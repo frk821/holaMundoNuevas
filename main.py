ada3198792c49db45f55d9bfe6eadcfe18e0ee80
209 <209@academicos.cesde.edu.co>
2022-08-05 20:08:42 -0500

    ntv1 se crea index y salida por consola

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hola mundo")
+#comentario de linea
+
+'''
+comentario de bloque
+'''
+
+#variables
+#Python es dinamicamente tipado
+
+nombre = "Frank"
+edad = 40
+estatura = 1.75
+hinchaDelVerde = True
